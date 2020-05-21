983466be9b207708d2a05e0983967050c77dfc30
parkjiminy <parkjiminy@gmail.com>
2020-05-21 19:02:20 +0900

    test

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'emforceapisdk',
-    version          = '0.1.1.1',
+    version          = '0.1.4.10',
     description      = 'Emforce Python sdk for media api',
     author           = 'Jimin Park',
     author_email     = 'parkjiminy@gmail.com',
     url              = 'https://github.com/emforcernd/emforceapisdk',
-    download_url     = 'https://github.com/emforcernd/emforceapisdk/archive/0.1.1.1.tar.gz',
-    install_requires    =  [],
+    # download_url     = 'https://github.com/emforcernd/emforceapisdk/archive/0.1.1.1.tar.gz',
+    install_requires    =  [
+        'google-api-python-client == 1.8.3',
+        'facebook_sdk == 4.0.0rc0',
+    ],
+    dependency_links =[
+        'https://github.com/mobolic/facebook-sdk.git@ffd9980700be48964d6a6a61144edb1c3ea29cff#egg=facebook_sdk-4.0.0rc0'
+    ],
     packages            = find_packages(exclude = []),
     keywords            = ['emforce','emforceapisdk'],
     python_requires     = '>=3',
